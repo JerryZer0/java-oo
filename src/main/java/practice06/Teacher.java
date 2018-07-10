df42daa49ff0af6ab5af3086d783ad57d0f346f4
Jerry <jerry.zhang@oocl.com>
2018-07-10 15:25:15 +0800

    practice 6 finished.test order is gradlew test

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,46 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    String name;
+    int age;
+    int klass;
+
+    Teacher(String name,int age,int klass){
+        super(name,age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+    Teacher(String name,int age){
+        super(name,age);
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String introduction = super.introduce();
+        String number = "";
+        if(klass == 0){
+            number = "No Class";
+        }else{
+            number = "Class "+klass;
+        }
+        introduction += " I am a Teacher. I teach " + number + ".";
+        return introduction;
+    }
+
 }
