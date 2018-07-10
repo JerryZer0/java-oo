148c955a973e4938f9919c92324270095550ccc0
Jerry <jerry.zhang@oocl.com>
2018-07-10 15:56:53 +0800

    practice 7 finished.test order is gradlew test

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,30 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    public String name;
+    public int age;
+    public Klass klass;
+
+    Student(String name, int age, Klass klass){
+        super(name,age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String introduction = super.introduce();
+        introduction += " I am a Student. I am at Class "+ klass.number +".";
+        return introduction;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,55 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    String name;
+    int age;
+    Klass klass;
+
+    Teacher(String name,int age,Klass klass){
+        super(name,age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+    Teacher(String name,int age){
+        super(name,age);
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String introduction = super.introduce();
+        String number = "";
+        if(klass == null){
+            number = "No Class";
+        }else{
+            number = "Class "+klass.number;
+        }
+        introduction += " I am a Teacher. I teach " + number + ".";
+        return introduction;
+    }
+
+    public String introduceWith(Student student){
+        String teach = "";
+        if(klass == student.klass)
+            teach = " I am a Teacher. I teach " + student.name + ".";
+        else
+            teach = " I am a Teacher. I don't teach " + student.name + ".";
+        return super.introduce()+ teach;
+    }
+
 }
