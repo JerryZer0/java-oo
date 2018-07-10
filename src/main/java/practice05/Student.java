2380e8858d10da3118910f4d95f5a5f6b42cc834
Jerry <jerry.zhang@oocl.com>
2018-07-10 15:17:25 +0800

    practice 5 finished,it is same with 4 the test order is gradlew test

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,30 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    public String name;
+    public int age;
+    public int klass;
+
+    Student(String name, int age, int klass){
+        super(name,age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        String introduction = super.introduce();
+        introduction += " I am a Student. I am at Class "+ klass +".";
+        return introduction;
+    }
 }
